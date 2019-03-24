ff02f899bbbaf4bc8da51dd79285b0f198bbb3d8
shashank-shark <shashankreddy231@gmail.com>
2019-03-24 16:11:20 +0530

    Initial Commit.
    Initial Commit.

---
+++ NumericalMath/Complex-Numbers/Basic.py
@@ -0,0 +1,18 @@
+# declaring complex num 1
+cNum1 = 1 + 2j
+
+# declaring complex num 2
+cNum2 = 2 + 6j
+
+# addition 
+cAdd = cNum1 + cNum2
+print ("CN1 + CN2 = ", cAdd)
+# subtraction
+cSub = cNum1 - cNum2
+print ("CN1 - CN2 = ", cSub)
+# multiplication
+cMul = cNum1 * cNum2
+print ("CN1 * CN2", cMul)
+# division
+cDiv = cNum1 / cNum2
+print ("CN1 / CN2", cDiv)
